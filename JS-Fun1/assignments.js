a0728ab0c16b7b827f3e653f1db3f80b08c76ce1
newdevmike22 <paola@dominicansoul.com>
2025-05-29 17:05:37 -0400

    Practice Assignment 1
    Practice Assignment 1 - Values and Variables..

---
+++ JS-Fun1/assignments.js
@@ -0,0 +1,10 @@
+// Assignment 1 - Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+// Log their values to the console.
+// Link it to your index.html file.
+
+let country = "Dominican Republic";
+let continent = "North America";
+let population = 11.33;
+let year = 2023;
+
+console.log(`The ${country} is located in ${continent}. As of ${year} it has a population of ${population} million.`);
