3f502c0913bce797c0898d16da5d1864c556946f
newdevmike22 <paola@dominicansoul.com>
2025-05-29 16:53:19 -0400

    JS Lesson 2
    JS Lesson 2 - Values and Variables.

--- JS-Fun1/app.js
+++ JS-Fun1/app.js
@@ -1,5 +1,25 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!");
-
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log("Spike");
+console.log(56);
+
+let firstName = "Mugsy";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let mugsy_lewis = "ML";
+let $age = 25;
+
+let person = "Vince";
+let PI = 3.1415;
+
+let myFirstJob = "Bus Boy";
+let myCurrentJob = "Web Developer";
+
+console.log(`${mugsy_lewis} just turned ${$age} today.`);
+console.log(`${person} finally knows that PI = ${PI}.`);
+console.log(`My very first job was as a ${myFirstJob}.`);
+console.log(`Now I'm a ${myCurrentJob}.`);
