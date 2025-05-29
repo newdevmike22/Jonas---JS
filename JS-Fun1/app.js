553842b30c99fee55d34737c83eaa4e65d4ec0ba
newdevmike22 <paola@dominicansoul.com>
2025-05-29 18:12:40 -0400

    JS Data Types & Assignment 2
    Learning JS Data Types & Assignment 2.

--- JS-Fun1/app.js
+++ JS-Fun1/app.js
@@ -1,3 +1,4 @@
+/*
 let js = "amazing";
 console.log(40 + 8 + 23 - 10);
 
@@ -23,3 +24,44 @@ console.log(`${mugsy_lewis} just turned ${$age} today.`);
 console.log(`${person} finally knows that PI = ${PI}.`);
 console.log(`My very first job was as a ${myFirstJob}.`);
 console.log(`Now I'm a ${myCurrentJob}.`);
+*/
+
+/* 7 Primitive Data Types */
+
+// 1. Number: Floating point numbers. Used for decimals and intergers.
+let age = 57;
+
+// 2. String: Sequence of characters. Used for text.
+let yourName = "Goku";
+
+// 3. Boolean: Logical type that can only be true or false. Used  for taking decisions.
+let isLegalAge = true;
+
+// 4. Undefined: Value taken by a variable that is not yet defined ('empty value).
+let children;
+
+// 5. Null: Also means 'empty value'.
+
+// 6. Symbol (ES2015: Value that is unique and can't be changed.
+
+// 7. BigInt (ES2020): Larger integers than the Number type can hold.
+
+let javaScriptIsCool = true;
+console.log(javaScriptIsCool);
+
+console.log(typeof true);
+console.log(typeof javaScriptIsCool);
+console.log(typeof 30);
+console.log(typeof "Mighty Mike");
+
+javaScriptIsCool = "Yabba-Dabba Do!";
+console.log(javaScriptIsCool);
+console.log(typeof javaScriptIsCool);
+
+let favoriteDish;
+console.log(favoriteDish);
+console.log(typeof favoriteDish);
+
+favoriteDish = "Pasta";
+console.log(favoriteDish);
+console.log(typeof favoriteDish);
--- JS-Fun1/assignments.js
+++ JS-Fun1/assignments.js
@@ -8,3 +8,13 @@ let population = 11.33;
 let year = 2023;
 
 console.log(`The ${country} is located in ${continent}. As of ${year} it has a population of ${population} million.`);
+
+// Assignment 2 - Declare a variable called isIsland and set its value according to your country. The variable should hold a Boolean value. Also declare a variable language, but don't assign it any value yet.
+// Log the types of isIsland, population, country and language to the console.
+let isIsland = true;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
