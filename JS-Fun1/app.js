2c62f2b59df9366de82a730d4c45d345afb0df61
newdevmike22 <paola@dominicansoul.com>
2025-05-31 21:36:33 -0400

    JS Type Conversion & Coercion
    Learning JS Type Conversion & Coercion.

--- JS-Fun1/app.js
+++ JS-Fun1/app.js
@@ -137,9 +137,8 @@ console.log(Biggie);
 console.log(`Hello
 I'm a string
 with multiple lines`);
-*/
 
-// If else statements
+// If / else statements
 const age = 15;
 
 if (age >= 18) {
@@ -159,3 +158,23 @@ if (birthYear <= 2000) {
 }
 
 console.log(century);
+*/
+
+// Type Conversion
+const inputYear = 1999;
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number("Spanky"));
+console.log(typeof NaN);
+
+console.log(String(30));
+
+// Type Coercion
+console.log("I am " + 23 + " years old.");
+console.log("23" + "10" + 3);
+console.log("23" * "2");
+
+let n = "1" + 1;
+n = n - 1;
+console.log(n);
